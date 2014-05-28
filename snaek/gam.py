ecc8237f4574d5107f5774b4f21a5807409d57fb
Hakon <hakonheggedal@gmail.com>
2014-05-28 13:06:28 +0200

    colors

--- snaek/gam.py
+++ snaek/gam.py
@@ -3,3 +3,16 @@ Created on 28. mai 2014
 
 @author: Håkon
 '''
+import pygame
+
+pygame.init()
+fpss = pygame.time.Clock()
+
+RED = pygame.Color(255,0,0)
+GREEN = pygame.Color(0,255,0)
+BLUE = pygame.Color(0,0,255)
+WHITE = pygame.Color(255,255,255)
+BLACK = pygame.Color(0,0,0)
+RED = pygame.Color(255,0,0)
+RED = pygame.Color(255,0,0)
+
